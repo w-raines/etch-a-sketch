974a439fc7ffa3e51a70247ebd43a323094637a0
w-raines <68299670+w-raines@users.noreply.github.com>
2024-11-02 22:41:25 -0400

    add functionality for colorful and opacity paints

--- script.js
+++ script.js
@@ -2,8 +2,12 @@
 
 const MAX_WIDTH = 1000;
 const MAX_HEIGHT = 1000;
+let mode = "Black";
+let size = 16;
 
-paintBoard(16);
+
+paintStaticContent();
+paintBoard(size);
 
 function paintBoard(squaresPerSide = 16) {
     let container = document.createElement('div');
@@ -18,9 +22,59 @@ function paintBoard(squaresPerSide = 16) {
     let containerChildren = container.children;
     for (let child of containerChildren) {
         child.addEventListener("mouseenter", () => {
-            child.style.backgroundColor = "black";
+            if (mode === "Opacity") {
+                if (child.dataset.opacityLevels === undefined) {
+                    child.dataset.opacityLevels = .1;
+                } else if (Number(child.dataset.opacityLevels) < 1) {
+                    child.dataset.opacityLevels = Number(child.dataset.opacityLevels) + .10;
+                } 
+                child.style.backgroundColor = `rgba(0, 0, 0, ${child.dataset.opacityLevels})`;
+            } else {
+                if (child.dataset.filled == undefined) {
+                    if (mode == "Black") {
+                        child.style.backgroundColor = "black";       
+                    } else {
+                        child.style.backgroundColor = randomColor();
+                    }
+                    child.dataset.filled = true;
+                }
+            }
         })
     }
+
+    let boardSize = document.createElement('p');
+    boardSize.innerText = `${squaresPerSide} x ${squaresPerSide}` 
+
+    document.querySelector('body').append(boardSize, container);
+    setSquareSizing(squaresPerSide);
+}
+
+function paintStaticContent() {
+    let modesContainer = document.createElement('div');
+    modesContainer.classList.add("modes-container")
+    let radioValues = ["Black", "Colorful", "Opacity"];
+    
+    radioValues.forEach(item => {
+        let input = document.createElement('input');
+        input.type = "radio";
+        input.id = item;
+        input.name = "modes"
+        if (item == "Black") input.checked = true;
+        
+        let label = document.createElement('label');
+        label.for = item;
+        label.innerHTML = item;
+        modesContainer.append(input, label)
+    })
+
+    modesContainer.addEventListener("click", (e) => {
+        console.log(e.target.id);
+        if (e.target && e.target.matches("input[type=radio]")) {
+            mode = e.target.id;
+            clearBoard();
+            paintBoard(size)
+        }
+    })
 
     // button to set grid sizing
     let button = document.createElement('button');
@@ -34,24 +88,18 @@ function paintBoard(squaresPerSide = 16) {
         } while(Number.isNaN(sizing) || sizing <= 0 || sizing > 100)
         
         if (sizing !== null) {
+            size = sizing;
             clearBoard();
             paintBoard(Number(sizing));
         }
     })
 
-    // displaying board size
-    let boardSize = document.createElement('p');
-    boardSize.innerText = `${squaresPerSide} x ${squaresPerSide}` 
-
-    document.querySelector('body').append(button, boardSize, container);
-    setSquareSizing(squaresPerSide);
+    document.querySelector('body').append(button, modesContainer);
 }
 
 function clearBoard() {
     let container = document.querySelector(".container");
-    let button = document.querySelector("button");
     let boardSize = document.querySelector("p");
-    button.remove();
     container.remove();
     boardSize.remove();
 }
