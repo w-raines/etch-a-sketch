8c25e2ff4d9f39c184a97c287862d68a1dce6835
w-raines <68299670+w-raines@users.noreply.github.com>
2024-11-02 21:43:17 -0400

    square base functionality working

--- script.js
+++ script.js
@@ -3,20 +3,57 @@
 const MAX_WIDTH = 1000;
 const MAX_HEIGHT = 1000;
 
-paintBoard();
+paintBoard(16);
 
 function paintBoard(squaresPerSide = 16) {
     let container = document.createElement('div');
     container.classList.add('container');
-
+    
     for (let i = 0; i < squaresPerSide * squaresPerSide; i++) {
         let square = document.createElement('div');
         square.classList.add('square');
         container.append(square);
     }
 
-    document.querySelector('body').appendChild(container);
+    let containerChildren = container.children;
+    for (let child of containerChildren) {
+        child.addEventListener("mouseenter", () => {
+            child.style.backgroundColor = "black";
+        })
+    }
+
+    // button to set grid sizing
+    let button = document.createElement('button');
+    button.innerText = "Change Grid Sizing";
+    button.addEventListener("click", () => {
+        let sizing;        
+        do {
+            sizing = prompt('Enter grid size (max 100): ', '');
+            if (sizing === null) break; 
+            sizing = Number(sizing); // done here cause Number(null) converts to 0.. javascript lmao
+        } while(Number.isNaN(sizing) || sizing <= 0 || sizing > 100)
+        
+        if (sizing !== null) {
+            clearBoard();
+            paintBoard(Number(sizing));
+        }
+    })
+
+    // displaying board size
+    let boardSize = document.createElement('p');
+    boardSize.innerText = `${squaresPerSide} x ${squaresPerSide}` 
+
+    document.querySelector('body').append(button, boardSize, container);
     setSquareSizing(squaresPerSide);
+}
+
+function clearBoard() {
+    let container = document.querySelector(".container");
+    let button = document.querySelector("button");
+    let boardSize = document.querySelector("p");
+    button.remove();
+    container.remove();
+    boardSize.remove();
 }
 
 function setSquareSizing(squaresPerSide) {
